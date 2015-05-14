6499e8f8ed8f7112ac05410a7438c678852b1f7d
Damian Dziaduch <damian.dziaduch@gforces.pl>
2015-05-14 16:12:07 +0200

    Added installer.

---
+++ baska.plugin.zsh
@@ -0,0 +1,3 @@
+alias pajaki='afplay ~/.oh-my-zsh/plugins/baska/samples/pajaki.wav'
+alias przestan='afplay ~/.oh-my-zsh/plugins/baska/samples/przestan.wav'
+alias przestan_kurwa='afplay ~/.oh-my-zsh/plugins/baska/samples/przestan.wav && afplay ~/baska/samples/pajaki.wav'
