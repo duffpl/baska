c8f663f3954740606bdaccd3b3cc9cb9688c3cbb
Błażej Pachut <blazej.pachut@gforces.pl>
2015-08-19 09:41:44 +0200

    Add new samples

--- baska.plugin.zsh
+++ baska.plugin.zsh
@@ -1,3 +1,4 @@
 alias pajaki='afplay ~/.oh-my-zsh/plugins/baska/samples/pajaki.wav'
 alias przestan='afplay ~/.oh-my-zsh/plugins/baska/samples/przestan.wav'
 alias przestan_kurwa='afplay ~/.oh-my-zsh/plugins/baska/samples/przestan.wav && afplay ~/baska/samples/pajaki.wav'
+alias swierszcze='afplay ~/.oh-my-zsh/plugins/baska/samples/swierszcze.wav
