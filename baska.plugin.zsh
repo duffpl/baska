ee2f0ee78e527412f774f657b53acfeb001b2f18
Rafal Miedziocha <rafal.miedziocha@gforces.pl>
2018-11-06 12:25:32 +0100

    dd

--- baska.plugin.zsh
+++ baska.plugin.zsh
@@ -4,6 +4,5 @@ alias przestan_kurwa='afplay ~/.oh-my-zsh/plugins/baska/samples/przestan.wav && 
 alias swierszcze='afplay ~/.oh-my-zsh/plugins/baska/samples/swierszcze.mp3'
 alias badum='afplay ~/.oh-my-zsh/plugins/baska/samples/badum.mp3'
 alias tada='afplay ~/.oh-my-zsh/plugins/baska/samples/tada.mp3'
-alias haha='afplay ~/.oh-my-zsh/plugins/baska/samples/haha.wav'
-alias swierszcze='afplay ~/.oh-my-zsh/plugins/baska/samples/swierszcze.wav'
+alias haha='afplay ~/.oh-my-zsh/plugins/baska/samples/haha_szydera.wav'
 alias upgrade_baska='cd ~/.oh-my-zsh/plugins/baska && git pull && cd -'
