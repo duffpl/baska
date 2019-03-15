eddf48028d79bdbef68bdc01537bfe4d2ebf8651
Błażej Pachut <bpachut@gmail.com>
2019-03-15 10:20:38 +0100

    Fame MMA !

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 git clone git@github.com:damiandziaduch/baska.git ~/.oh-my-zsh/plugins/baska
 cp ~/.zshrc ~/.zshrc-baska-backup
 perl -p -i -e 's/plugins=\((.*)\)/plugins=($1 baska)/g' ~/.zshrc
